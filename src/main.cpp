e8bbdaf167f277d854b8f7938ae40b6cbb76787d
Ville Välimaa <ville.valimaa@reaktor.com>
2018-02-17 17:03:50 +0200

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,16 @@
 #include "SDL.h"
 #include "renderer.h"
 
-const int SCREEN_FPS = 120;
+const int SCREEN_FPS = 60;
 const int SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
 
 int main(int argc, char** argv) {
     chip8_system chip8;
+    chip8.init();
+    chip8.load("roms/PONG");
 
     std::unique_ptr<gpu_renderer> gRenderer = std::make_unique<gpu_renderer>();
     gRenderer->init();
-
-    chip8.init();
-    chip8.load("roms/PONG");
 
     for(;;)
     {
@@ -27,12 +26,8 @@ int main(int argc, char** argv) {
         }
 
         chip8.setKeys();
-
-        // TEMP SOLUTION! REPLACE LATER WITH OPENGL RENDERER
-        //std::this_thread::sleep_for (std::chrono::milliseconds(16));
         SDL_Delay(SCREEN_TICKS_PER_FRAME);
     }
-
 
     return 0;
 }
